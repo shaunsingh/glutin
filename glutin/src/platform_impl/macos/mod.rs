0910f9fe8c626a0a9f8b6839c670512792e34532
shaunsingh <shaunsingh0207@gmail.com>
2023-03-05 20:32:14 -0500

    test2

--- glutin/src/platform_impl/macos/mod.rs
+++ glutin/src/platform_impl/macos/mod.rs
@@ -51,7 +51,7 @@ impl Context {
         pf_reqs: &PixelFormatRequirements,
         gl_attr: &GlAttributes<&Context>,
     ) -> Result<(Window, Self), CreationError> {
-        // let transparent = wb.window.transparent;
+        let transparent = wb.transparent();
         let win = wb.build(el)?;
 
         let share_ctx = gl_attr.sharing.map_or(nil, |c| *c.get_id());
@@ -125,7 +125,6 @@ impl Context {
                 appkit::NSOpenGLContextParameter::NSOpenGLCPSwapInterval,
             );
 
-            /*
             if transparent {
                 let mut opacity = 0;
                 CGLSetParameter(
@@ -134,7 +133,6 @@ impl Context {
                     &mut opacity,
                 );
             }
-            */
 
             CGLEnable(gl_context.CGLContextObj() as *mut _, kCGLCECrashOnRemovedFunctions);
 
